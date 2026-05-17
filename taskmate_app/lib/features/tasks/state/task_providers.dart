b14ba2e20f56aa7ebf92717e88027b087c61a080
Ashikur Rahman Nihal <arnihal.cs@gmail.com>
2026-05-17 23:26:59 +0600

    flutter package update pubspec

--- taskmate_app/lib/features/tasks/state/task_providers.dart
+++ taskmate_app/lib/features/tasks/state/task_providers.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod/legacy.dart';
 import 'package:uuid/uuid.dart';
 import '../../../core/storage/hive_bootstrap.dart';
 import '../data/task_repository.dart';
